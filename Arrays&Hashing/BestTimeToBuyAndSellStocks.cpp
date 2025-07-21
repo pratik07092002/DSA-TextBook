1794b86fae6b6797b7eec19b23ae6419ee3e0069
Pratik Gaikwad <pratikgaikwad072002@gmail.com>
2025-07-21 22:16:27 +0530

    BestTimeToBuyandSellStock

---
+++ Arrays&Hashing/BestTimeToBuyAndSellStocks.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maxProfitBruteForce(vector<int>& nums)
+{
+    int maxDay = 0;
+    int maxValue = 0;
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int profit = nums[j] - nums[i];
+            if(profit > maxValue){
+  maxValue = profit;
+            }
+          
+        }
+    }
+
+    return maxValue;
+}
+
+int maxProfitOptimal(vector<int>& nums)
+{
+    int minPrice = INT_MAX; 
+        int maxProfit = 0;      
+
+        for (int price : nums) {
+            if (price < minPrice) {
+                minPrice = price; 
+            } else {
+                int profit = price - minPrice; 
+                maxProfit = max(maxProfit, profit); 
+            }
+        }
+
+        return maxProfit;
+}
+int main(int argc, char const *argv[])
+{
+    vector<int> nums = {1, 2, 3, 4, 5};
+    int answer = maxProfitBruteForce(nums);
+    cout<<answer;
+    return 0;
+}
