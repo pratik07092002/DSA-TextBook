655a943797fd4e969a71b6634722ed5cf3ff4922
Pratik Gaikwad <pratikgaikwad072002@gmail.com>
2025-07-27 22:37:12 +0530

    missing number problem solved with brute force

---
+++ Arrays&Hashing/MissingNumber.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int missingNumberBruteForce(vector<int>& nums){
+int n = nums.size();
+int m = 0;
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n - i - 1; j++)
+    {
+        if(nums[j] > nums[j+1]){
+            swap(nums[j],nums[j+1]);
+        }
+    }  
+}
+  for (int i = 0; i < n; i++)
+    {
+        if(nums[i]+1 != nums[i+1]){
+            m = nums[i]+1;
+        break;
+        }
+    }
+
+
+    return m;
+
+}
+int main(int argc, char const *argv[])
+{
+    vector<int> nums = {1,0,3};
+    int number = missingNumberBruteForce(nums);
+    cout<<"Missing "<< number;
+    return 0;
+}
