bd84713f6f71995ef0a8dd3eb9f3a04b7bb089a3
Pratik Gaikwad <pratikgaikwad072002@gmail.com>
2025-07-30 21:44:31 +0530

    Number Appearing Once

---
+++ Arrays&Hashing/numberAppearingOnce.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int numbersAppearingOnceHashing(vector<int> nums){
+    int answer = 0;
+        unordered_map<int,int> umap;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        umap[nums[i]]++;
+
+    }
+for( const auto& pair : umap){
+    if(pair.second == 1){
+        return pair.first;
+    }
+}
+   return -1;
+}
+int main(int argc, char const *argv[])
+{
+    vector<int> nums = {1,2,3,4,5,6};
+    int answer = numbersAppearingOnceHashing(nums);
+    cout<<answer;
+
+    
+    return 0;
+}
